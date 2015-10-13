ad108af4113243606e5771d6f2048aab6bbe1129
zstapic <zlatko.stapic@foi.hr>
2015-10-14 01:59:28 +0200

    Creating application menu  - define menu in resources (main.xml and strings.xml)  - show menu by overriding onCreateOptionsMenu  - handle user's menu item selection by overriding onOptionsItemSelected

--- app/src/main/java/hr/foi/air/discountlocator/MainActivity.java
+++ app/src/main/java/hr/foi/air/discountlocator/MainActivity.java
@@ -1,7 +1,10 @@
 package hr.foi.air.discountlocator;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,40 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    /**
+     * Overides extended method and inflates menu. After adding this method, user will be able to show menu.
+     * @param menu The reference to the object that should be inflated according to menu definition in resources.
+     * @return True if everything is OK.
+     */
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    /**
+     * Handles menu item clicks by overriding extended method.
+     * @param item Contains reference to the item that user clicked on.
+     * @return True if event is handled correctly.
+     */
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        //Switch on item id.
+        //Currently there are no actions to perform.
+        int id = item.getItemId();
+        switch (id)
+        {
+            case R.id.action_search:
+                break;
+            case R.id.action_settings:
+                break;
+        }
+
+        //Temporary shows message that click is handled.
+        Toast.makeText(this, "Menu item " + item.getTitle() + " clicked!", Toast.LENGTH_SHORT).show();
+        return true;
     }
 }
