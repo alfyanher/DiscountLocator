581d39ece039dad6fb875916887556a157b115dd
zstapic <zlatko.stapic@foi.hr>
2015-10-14 02:50:13 +0200

    For those who want more  - Adding back button into application action bar  - get support action bar  - setDisplayHomeAsUpEnabled -> shows back/home option button in action bar  - handle click on this button the same way as click on other option buttons  - EXERCISES 1 COMPLETED

--- app/src/main/java/hr/foi/air/discountlocator/DiscountDetailsActivity.java
+++ app/src/main/java/hr/foi/air/discountlocator/DiscountDetailsActivity.java
@@ -1,7 +1,10 @@
 package hr.foi.air.discountlocator;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
 
 public class DiscountDetailsActivity extends AppCompatActivity {
 
@@ -9,5 +12,33 @@ public class DiscountDetailsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_discount_details);
+
+        //Showing back button in the action bar of the activity
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+        //Note: Think why we had to use getSupportActionBar() method instead of getActionBar()?
+    }
+
+    /**
+     * Overriding extended method to handle onOptionsItemsSelected event of back button in the
+     * action bar of the activity. Other options are handled by calling original (overridden) method.
+     * @param item A reference to selected options item.
+     * @return True if event is successfully handled.
+     */
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                // back button (or app icon) in action bar clicked; go to main activity
+                Intent intent = new Intent(this, MainActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                //Note: Find out what is Intent FLAG used for. What is this flag user for?
+                startActivity(intent);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
