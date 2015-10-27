504907c362bc6688dc1302b05e5a0949bb3d1ac8
zstapic <zlatko.stapic@foi.hr>
2015-10-27 11:04:34 +0100

    03 Communication with web service  - the communication with web service is achieved by using HttpURLConnection class  - the communication is performed in asynchronous do in background method  - the procedure included following steps    - define http header and connection    - if using post method, send post parameters    - if expecting results, read web service response - forward the results to synchronous onPostExecute method

--- ws/src/main/java/hr/foi/air/discountlocator/ws/WebServiceAsyncTask.java
+++ ws/src/main/java/hr/foi/air/discountlocator/ws/WebServiceAsyncTask.java
@@ -2,13 +2,65 @@ package hr.foi.air.discountlocator.ws;
 
 import android.os.AsyncTask;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 public class WebServiceAsyncTask extends AsyncTask <WebServiceParams, Void, AsyncTaskInnerResults> {
 
     @Override
     protected AsyncTaskInnerResults doInBackground(WebServiceParams[] params) {
         AsyncTaskInnerResults asyncTaskInnerResult = new AsyncTaskInnerResults();
 
-        //implement ws call here
+        asyncTaskInnerResult.wsResult = "";
+
+        String url = "http://cortex.foi.hr/mtl/courses/air/"
+                + params[0].serviceName + ".php";
+
+        String urlParameters = "";
+        urlParameters += "method=" + params[0].methodName;
+        urlParameters += "&" + "json=" + params[0].jsonParams;
+        byte[] postData = urlParameters.getBytes();
+
+        HttpURLConnection c = null;
+
+        try {
+            c = (HttpURLConnection)(new URL(url)).openConnection();
+            c.setDoOutput(true);
+            c.setDoInput(true);
+            c.setUseCaches(false);
+            c.setRequestMethod("POST");
+            c.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+            c.setRequestProperty("charset", "utf-8");
+            c.setRequestProperty("Content-Length", ""+postData.length);
+
+            //sending post parameters
+            DataOutputStream dw = new DataOutputStream(c.getOutputStream());
+            dw.write(postData);
+
+            //reading web service response
+            InputStream is = new BufferedInputStream(c.getInputStream());
+            BufferedReader dr = new BufferedReader(new InputStreamReader(is));
+            String line = "";
+            while ((line = dr.readLine()) != null)
+            {
+                asyncTaskInnerResult.wsResult += line;
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        finally {
+            if (c != null)
+                c.disconnect();
+        }
+
+        asyncTaskInnerResult.handler = params[0].resultHandler;
+        asyncTaskInnerResult.targetAttribute = params[0].targetAttribute;
 
         return asyncTaskInnerResult;
     }
