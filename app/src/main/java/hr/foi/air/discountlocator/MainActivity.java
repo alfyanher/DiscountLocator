218f5bca0c34cf6e2a35cf9d31dd3d4c54f6b550
isvogor-foi <isvogor@foi.hr>
2015-11-16 14:02:47 +0100

    05 Custom Toolbar
    - customized toolbar added to the action bar
    - added missing strings, additional code is added to display the hamburger icon
    - rotation is handled (so the toolbar is displayed corectly) - onConfigurationChanged and onPostCreate (to display it)

--- app/src/main/java/hr/foi/air/discountlocator/MainActivity.java
+++ app/src/main/java/hr/foi/air/discountlocator/MainActivity.java
@@ -1,8 +1,12 @@
 package hr.foi.air.discountlocator;
 
 import android.app.FragmentTransaction;
+import android.content.res.Configuration;
 import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toast;
@@ -13,6 +17,9 @@ import hr.foi.air.discountlocator.core.DataLoader;
 import hr.foi.air.discountlocator.loaders.WebServiceDataLoader;
 
 public class MainActivity extends AppCompatActivity {
+    private DrawerLayout mDrawer;
+    private Toolbar mToolbar; // from android.support.v7.widget.Toolbar;
+    private ActionBarDrawerToggle mDrawerToggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,10 +27,24 @@ public class MainActivity extends AppCompatActivity {
         ActiveAndroid.initialize(this);
         setContentView(R.layout.activity_main);
 
+        // replace the ActionBar with the Toolbar
+        mToolbar= (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(mToolbar);
+        // find the drawer layout
+        mDrawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+
+        mDrawerToggle = setupDrawerToggle();
+        mDrawer.setDrawerListener(mDrawerToggle);
+
         DiscountListFragment dlf = new DiscountListFragment();
         FragmentTransaction fm = getFragmentManager().beginTransaction();
         fm.replace(R.id.fragment_container, dlf);
         fm.commit();
+    }
+
+    // ActionBarDrawerToggle from support v.7
+    private ActionBarDrawerToggle setupDrawerToggle() {
+        return new ActionBarDrawerToggle(this, mDrawer, mToolbar, R.string.drawer_open,  R.string.drawer_close);
     }
 
     /**
@@ -68,5 +89,17 @@ public class MainActivity extends AppCompatActivity {
         } else {
             super.onBackPressed();
         }
+    }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        mDrawerToggle.syncState();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        mDrawerToggle.onConfigurationChanged(newConfig);
     }
 }
